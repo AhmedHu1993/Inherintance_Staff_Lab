425bac1d6642073fef59f42e2bf5b17126c9c7c9
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-10-14 15:14:54 +0100

    director class and test

---
+++ src/test/java/DirectorTest.java
@@ -0,0 +1,35 @@
+import management.Director;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DirectorTest {
+
+    Director director;
+
+    @Before
+    public void before(){
+        director = new Director("Doola", 56789, 6000, "HR", 250000);
+    }
+
+    @Test
+    public void testDirectorProperties(){
+        assertEquals("Doola", director.getName());
+        assertEquals(56789, director.getNiNum());
+        assertEquals(6000, director.getSalary());
+        assertEquals("HR", director.getDeptName());
+        assertEquals(250000, director.getBudget(), 10);
+    }
+
+    @Test
+    public void testRaiseDirectorSalary(){
+        director.raiseSalary(260.5);
+        assertEquals(6260.5, director.getSalary(), 0.5);
+    }
+
+    @Test
+    public void testDirectorPayBonus(){
+        assertEquals(60, director.payBonus(), 0);
+    }
+}
